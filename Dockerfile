ae025246c7430a12d977d2d278f3e8d3625ca876
Ismaël Lajaaiti <ismael.lajaaiti@gmail.com>
2025-08-08 17:50:49 +0200

    Download data with kaggle

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,12 @@ WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-
+RUN mkdir -p data
+ARG KAGGLE_USERNAME
+ARG KAGGLE_KEY
+ENV KAGGLE_USERNAME=${KAGGLE_USERNAME}
+ENV KAGGLE_KEY=${KAGGLE_KEY}
+RUN kaggle datasets download maharshipandya/-spotify-tracks-dataset -p data --unzip
 COPY . .
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,25 +1,36 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import os
 
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
-)
+port = int(os.environ.get("PORT", 8050))
+
+df = pd.read_csv("data/dataset.csv")
 
 app = Dash()
 
-# Requires Dash 2.17.0 or later
 app.layout = [
-    html.H1(children="Title of Dash App", style={"textAlign": "center"}),
-    dcc.Dropdown(df.country.unique(), "Canada", id="dropdown-selection"),
-    dcc.Graph(id="graph-content"),
+    html.H1(children="Anti-Style", style={"textAlign": "center"}),
 ]
 
 
-@callback(Output("graph-content", "figure"), Input("dropdown-selection", "value"))
-def update_graph(value):
-    dff = df[df.country == value]
-    return px.line(dff, x="year", y="pop")
+# Show first 5 rows as HTML table
+app.layout = html.Div(
+    [
+        html.H1("Anti-Style - Preview", style={"textAlign": "center"}),
+        html.Table(
+            [
+                html.Thead(html.Tr([html.Th(col) for col in df.columns])),
+                html.Tbody(
+                    [
+                        html.Tr([html.Td(df.iloc[i][col]) for col in df.columns])
+                        for i in range(min(len(df), 5))  # Only first 5 rows
+                    ]
+                ),
+            ]
+        ),
+    ]
+)
 
 
 if __name__ == "__main__":
